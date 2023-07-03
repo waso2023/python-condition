0cc4b2b4623fdee677b45b98c7429498201108cd
Your Name <evansomondi256@gmail.com>
2023-07-03 16:09:48 +0300

    python-condition

--- conditions.py
+++ conditions.py
@@ -1,38 +1,45 @@
-marks = float(input("marks"))
+
+marks = int(input("marks:"))
 
 if marks > 100:
-    print("error")
+  print("error")
 
-    if marks > 91 < 100:
-        print("Grade A plain")
+elif marks >80 <100:
+    grade ="A plain"
 
-if marks > 86 < 90:
- print("Grade A-")
+elif marks >75 <79:
+    grade = "A-"
 
- if marks > 80 < 85:
-  print("Grade B+ ")
+elif marks>70 <74:
+    grade ="B+"
 
+elif marks >65 < 69:
+    grade ="B plain"
 
-if marks > 70 < 79:
-    print("Grade B plain")
+elif marks >60 <64:
+    grade ="B-"
 
+elif marks >55 <59:
+    grade ="C+"
 
-if marks >60 <69:
-    print("Grade B-")
+elif marks >50 <54:
+    grade ="C plain"
 
-elif marks >50 <59:
-    print("Grade C+")
+elif marks > 45 < 49:
+    grade = "C-"
 
-elif marks >40 <49:
-    print("Grade C plain")
+elif marks > 40 < 44:
+    grade = "D+"
 
-elif marks > 30 <39:
-    print("Grade C- ")
+elif marks > 35 < 39:
+    grade = "D plain"
 
-elif marks > 20 <29:
-    print("Grade D+ ")
+elif marks > 30 < 34:
+    grade = "D-"
 
-elif marks > 0 < 19:
-        print("Grade D- ")
+elif marks  > 0 < 29:
+    grade = "E"
 
-else:print("error")
+else:grade = "error"
+
+print( grade)
