c79089d6c43d2dd62ccebeef2ed980de788b823d
Your Name <evansomondi256@gmail.com>
2023-07-03 14:16:41 +0300

    python condition

---
+++ conditions.py
@@ -0,0 +1,38 @@
+marks = float(input("marks"))
+
+if marks > 100:
+    print("error")
+
+    if marks > 91 < 100:
+        print("Grade A plain")
+
+if marks > 86 < 90:
+ print("Grade A-")
+
+ if marks > 80 < 85:
+  print("Grade B+ ")
+
+
+if marks > 70 < 79:
+    print("Grade B plain")
+
+
+if marks >60 <69:
+    print("Grade B-")
+
+elif marks >50 <59:
+    print("Grade C+")
+
+elif marks >40 <49:
+    print("Grade C plain")
+
+elif marks > 30 <39:
+    print("Grade C- ")
+
+elif marks > 20 <29:
+    print("Grade D+ ")
+
+elif marks > 0 < 19:
+        print("Grade D- ")
+
+else:print("error")
